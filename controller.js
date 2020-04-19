09387f1c84930e9d2ef89f174e7f0ac312cb693a
latifahiriyani <latifahiriyani98@gmail.com>
2020-04-19 14:04:36 +0700

    berhasil menampilkan data semua montir

--- controller.js
+++ controller.js
@@ -6,3 +6,15 @@ var connection = require('./koneksi');
 exports.index = function(req, res){
     response.ok("Aplikasi REST API ku berjalan",res)
 };
+
+//menampilkan semua data montir
+exports.tampilsemuamontir = function(req,res){
+    connection.query('SELECT * FROM t_montir', function(error, rows, fileds){
+        if(error){
+            connection.log(error);
+        }else {
+            response.ok(rows,res)
+        }
+
+    });  
+};
--- server.js
+++ server.js
@@ -12,5 +12,6 @@ var routes =require('./routes');
 routes(app);
 
 app.listen(3000, () => {
-    console.log('Server started on port');
+    console.log('Server started on port'
+    );
 });
